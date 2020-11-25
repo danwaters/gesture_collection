ca004f001337f5c6d0e817e721fd3586c093cf74
Dan Waters <dawate@microsoft.com>
2020-11-24 19:56:44 -0600

    Defaulting to a debug build
    https://github.com/flutter/flutter/issues/58247#issuecomment-636500680

--- gesture_collection_app/android/app/appcenter-post-clone.sh
+++ gesture_collection_app/android/app/appcenter-post-clone.sh
@@ -15,7 +15,7 @@ export PATH=`pwd`/flutter/bin:$PATH
 
 flutter channel stable
 flutter doctor
-flutter build apk --release
+flutter build apk --debug
 
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_<
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/debug/app-debug.apk $_<
